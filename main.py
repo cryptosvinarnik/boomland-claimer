4b7d8a166d1947d564303c05743b95ca02c62f80
cryptosvinarnik <na4030680@gmail.com>
2023-03-24 17:53:26 +0200

    gas estimating and pretty log without keys

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from loguru import logger
-from web3 import Web3
+from web3 import Account, Web3
+from web3.exceptions import ContractLogicError
 from web3.middleware import geth_poa_middleware
 
 
@@ -10,8 +11,11 @@ def init_web3(RPC_URL: str) -> Web3:
     return w3
 
 
-def send_tx(w3: Web3, private_key: str, contract_address, data) -> str:
-    account = w3.eth.account.privateKeyToAccount(private_key)
+def init_accounts(w3: Web3, private_keys: list) -> list[Account]:
+    return [w3.eth.account.privateKeyToAccount(private_key) for private_key in private_keys]
+
+
+def send_tx(w3: Web3, account: Account, contract_address, data) -> str:
     nonce = w3.eth.get_transaction_count(account.address, "pending")
 
     tx = {
@@ -25,6 +29,8 @@ def send_tx(w3: Web3, private_key: str, contract_address, data) -> str:
         "data": data
     }
 
+    w3.eth.estimate_gas(tx)
+
     signed_tx = account.sign_transaction(tx)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction).hex()
 
@@ -37,18 +43,29 @@ def main():
     if not w3.isConnected():
         logger.error("Can't connect to RPC")
         return
-    
-    BOOMLAND_CONTRACT = Web3.toChecksumAddress("0x3a1f862d8323138f14494f9fb50c537906b12b81")
+
+    BOOMLAND_CONTRACT = Web3.toChecksumAddress(
+        "0x3a1f862d8323138f14494f9fb50c537906b12b81")
     BOOMLAND_CLAIM_DATA = "0x359cf2b7"
 
-    private_keys = open(input("File with private keys >> ")).read().splitlines()
+    private_keys = open(input("File with private keys >> ")
+                        ).read().splitlines()
 
-    for private_key in private_keys:
+    logger.info("Initializing accounts...")
+    try:
+        accounts = init_accounts(w3, private_keys)
+    except Exception as e:
+        logger.error(f"Error initializing accounts: {e}")
+        return
+
+    for account in accounts:
         try:
-            tx_hash = send_tx(w3, private_key, BOOMLAND_CONTRACT, BOOMLAND_CLAIM_DATA)
-            logger.success(f"Sent tx from {private_key} with {tx_hash=}")
+            tx_hash = send_tx(w3, account, BOOMLAND_CONTRACT, BOOMLAND_CLAIM_DATA)
+            logger.success(f"Sent tx from {account.address} with {tx_hash=}")
+        except ContractLogicError as e:
+            logger.warning(f"Error simulate tx from {account.address}: {e}. Maybe, claim for this address unavailable.")
         except Exception as e:
-            logger.error(f"Error sending tx from {private_key}: {e}")
+            logger.error(f"Error sending tx from {account.address}: {e}")
 
 
 if __name__ == "__main__":
